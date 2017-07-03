b83df5c9b1063db941c9aa575d7413bcda1e7082
Dom Hastings <dom111@users.noreply.github.com>
2017-07-03 21:46:47 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # bash-todo
+
 A customisable bash/Perl script that shows your TODO list in your terminal window
+
+Inspired by this post on reddit:
+
+    https://www.reddit.com/r/bash/comments/6kvl4m/to_do_list_display_it_in_the_top_of_the_terminal/
+
+[@whetu](https://www.reddit.com/user/whetu) said: "I suspect it's maybe do-able with some `tput` trickery combined with `$PROMPT_COMMAND`"
+
+That made me wonder _who_ do-able it was. Very do-able.
+
+## Usage
+
+Place `TODO.sh` somewhere accessible (`~/bin/TODO.sh` for instance), set up your `PROMPT_COMMAND` variable:
+
+    PROMPT_COMMAND=~/bin/TODO.sh
+
+Add some `TODO`s:
+
+    ~/bin/TODO.sh "I need to do this thing"
+    ~/bin/TODO.sh "and I have to do this thing too"
+
+or if you're feeling really crazy:
+
+    grep -Rn 'TODO\|FIXME' ~/project | perl -pe 's/:[^:]+TODO/:TODO/' | ~/bin/TODO.sh -s
+
+which will add all `TODO`/`FIXME` from `~/project`. Yeah, you probably don't wanna do that.
+
+From then on you should see your list appearing in the top right-hand corner of your terminal. It's numbered so you can run things like:
+
+    ~/bin/TODO.sh -t 4   # toggles item 4 as done/not done
+    ~/bin/TODO.sh -d 2   # deleted item 2 from your list
+
+This script assumes you want to work on `~/TODO`, but you can change the path to the file in `TODO.sh` itself, along with a few other settings.
---
+++ TODO.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+
+# customisation
+export max_width=".5";     # what fraction of the width of the screen can be used
+export max_height="1";     # what fraction of the height of the screen can be used
+export style="44;97";      # specify colours for the main window
+export done_style="90";    # specify alternative colors for items that are done
+
+# colour only settings
+colour_header=" {fill}";
+colour_footer=" {fill}";
+colour_line_prefix="   ";
+colour_line_suffix="   ";
+
+# text only settings
+text_header="+-{fill}+\n|{center}TODO{/center}{right}|{/right}\n+-{fill}+";
+text_footer="+-{fill}+";
+text_line_prefix="| ";
+text_line_suffix=" |";
+
+# header and footer strings can have the following formatters applied:
+#   x{fill}                 - fills the line with 'x' excepting any chars before or after
+#   {center}text{center}    - prints 'text' in the center of the line
+#   {right}text{/right}     - right-aligns 'text'
+
+# main
+export cols=$(tput cols);
+export rows=$(tput lines);
+
+file=~/TODO;
+
+perl_start=$(grep -n '#!perl' $0 | tail -1 | awk -F: '{print$1}');
+format_script=$(awk -F\n "{if(NR>=$perl_start){print}}" "$0");
+
+function usage {
+    echo "Usage:
+    $0 [text]           - add item
+    $0 -t [n]           - toggle item #n done
+    $0 -d [n]           - delete item #n
+    $0 -s               - add items from stdin (useful in pipes)
+";
+}
+
+function _format {
+    export line_prefix=$1;
+    export line_suffix=$2;
+    export all_prefix=$3;
+    export all_suffix=$4;
+    export done_prefix=$5;
+    export header=$6;
+    export footer=$7;
+
+    perl -e "$format_script" < $file;
+}
+
+if [[ "$@" = *"-h"* || "$@" = *"--h"* ]]; then
+    usage;
+    exit 0;
+fi
+
+if [[ -n "$@" ]]; then
+    if [[ "$1" = "-s" ]]; then
+        # slurp from stdin
+        echo "$(cat -)" >> $file;
+    elif [[ "$1" = "-t" ]]; then
+        # toggle $2 done
+        if [[ -n "$2" ]]; then
+            perl -pli -e "$.==$2?/\bdone$/i?s/\bdone\b//i|s/^\s+|\s+$//g:s/$/ done/:0" $file;
+        fi
+    elif [[ "$1" = "-d" ]]; then
+        # delete $2
+        if [[ -n "$2" ]]; then
+            perl -nli -e "$.==$2||print" $file;
+        fi
+    else
+        # assume lazy add mode
+        echo "$@" >> $file;
+    fi
+
+    exit 0;
+fi
+
+if [[ -e $file ]]; then
+    if [[ $(wc -l $file | awk '{print$1}') = 0 ]]; then
+        exit 0;
+    fi
+else
+    exit 1;
+fi
+
+tput sc;
+
+if [ $(tput colors) -ge 8 ]; then
+    _format "$colour_line_prefix" "$colour_line_suffix" "\x1b[${style}m" "\x1b[0m" "\x1b[${done_style}m" "$colour_header" "$colour_footer";
+else
+    _format "$text_line_prefix" "$text_line_suffic" "" "" "" "$text_header" "$text_footer";
+fi
+
+tput rc;
+
+exit 0;
+
+#!perl
+use strict;
+
+my @items = <>;
+chomp(@items);
+
+my $line_prefix = $ENV{line_prefix};
+my $line_suffix = $ENV{line_suffix};
+my $all_prefix = $ENV{all_prefix};
+my $all_suffix = $ENV{all_suffix};
+my $done_prefix = $ENV{done_prefix};
+my $header = $ENV{header};
+my $footer = $ENV{footer};
+my $max_width = int($ENV{cols} * $ENV{max_width});
+my $max_height = int($ENV{rows} * $ENV{max_height});
+my $longest_item;
+$longest_item = length > $longest_item ? length : $longest_item for @items;
+my $number_of_items = @items;
+my $left_column_length = length($number_of_items) + 1;
+my $max_right_column_width = $max_width - ($left_column_length + 1);
+my $max_right_column_content_width = $max_right_column_width - (length($line_prefix) + length($line_suffix));
+my $longest_line_length = $longest_item + length($line_prefix) + length($line_suffix);
+my $actual_line_length = ($longest_line_length, $max_right_column_width)[$longest_line_length > $max_right_column_width];
+my $full_width = $left_column_length + 1 + $actual_line_length;
+my $start_column = $ENV{cols} - $full_width + 1;
+my ($row_counter, $item_number, $adjusted_content_width);
+
+($header, $footer) = map {
+    join($/, map {
+        s/(.)\{fill\}/$1 x($max_width - (length($`) + length($')))/e;
+        s!\{center\}(.+)\{/center\}!$"x(int($max_width / 2) - length($`) - int(length($1) / 2)).$1!e;
+        s!\{right\}(.+)\{/right\}!$"x($max_width-length($`) - length($1)).$1!e;
+        $_
+    } split/\n/, $_);
+} $header, $footer;
+
+$max_height -= split(/\n/, "$header
+$footer");
+
+if (@items > $max_height) {
+    my $missing = (@items - ($max_height - 1));
+    @items = (@items[0..($max_height - 2)], "  $missing more items");
+}
+
+map {
+    ++$row_counter;
+    print "\x1b[$row_counter;${start_column}f$_"
+}
+map {
+    "$all_prefix$_$all_suffix"=~s/\\x1b/\x1b/gr
+}
+split(/\n/,$header),(map{
+    $adjusted_content_width = $max_right_column_content_width;
+
+    map{
+        $adjusted_content_width += length
+    } /\\x1b\[[^m]+m/g;
+
+    ++$item_number;
+    sprintf "$line_prefix%$left_column_length.${left_column_length}s %-$adjusted_content_width.${adjusted_content_width}s$line_suffix",!/\d+ more items/&&"${item_number}.",$_
+}
+map {
+    (/\bdone\b/&&$done_prefix).((length > $max_right_column_content_width) ? substr($_, 0, $max_right_column_content_width - 3).'...' : $_)
+} @items), split(/\n/, $footer);
